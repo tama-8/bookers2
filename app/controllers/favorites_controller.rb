f2695f3dd4179842ce555c9fbb34e3aa9f09653a
Kaori Tamaki <tamatora0101@gmail.com>
2024-07-05 23:09:53 +0900

    change:いいね機能の非同期通信化(Ajax)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,15 +3,22 @@ class FavoritesController < ApplicationController
 
   def create
    @favorite = current_user.favorites.create(book_id: params[:book_id])
-   redirect_back(fallback_location: root_path)
+   @favorite.save
+  #  redirect_back(fallback_location: root_path)
+  respond_to do |format|
+    format.html { redirect_to 'books/index' }
+    format.js
+  end
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-
-
+  @book = Book.find(params[:book_id])
   @favorite = current_user.favorites.find_by(user_id: current_user.id, book_id: params[:book_id])
    @favorite.destroy
-   redirect_back(fallback_location: root_path)
+  #  redirect_back(fallback_location: root_path)
+  respond_to do |format|
+    format.html { redirect_to 'books/index'}
+    format.js
+  end
   end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,15 +2,23 @@ class RelationshipsController < ApplicationController
 
   before_action :authenticate_user!
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+    #redirect_to request.referer
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    redirect_to  request.referer
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+    #redirect_to  request.referer
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
 end
